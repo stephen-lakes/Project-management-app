53dfa4b01c5d5b511b4db7cce927da8b502af4df
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-01 12:41:47 +0100

    added script to toggle nav links

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -10,4 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
     totalCost.innerHTML = this.value * 17.5
   }
 
+  navToggler = document.getElementById('btn');
+  navToggler.addEventListener('click', function(){
+    var navContainer = document.getElementById('nav')
+    if (navContainer.style.display !== "block"){
+      navContainer.style.display = "block"
+    }else {
+      navContainer.style.display = "none"
+    }
+  }); 
+
+
 }, false);
+
+  
