b5341d347e6bed2a81446a2b4433e4d7d5ab290b
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-01 04:49:07 +0100

    Update: made the slider dynamic

---
+++ Assets/js/index.js
@@ -0,0 +1,8 @@
+var slide = document.getElementById('slide')
+var sliderNum = document.getElementById('num-of-users');
+var totalCost = document.getElementById('total-cost');
+
+slide.onchange = function() {
+  sliderNum.innerHTML = this.value;
+  totalCost.innerHTML = this.value * 17.5
+}
