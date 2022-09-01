d8f00aefb790cff58bb94cbfa714ec9c0f7b7d67
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-09-01 04:57:14 +0100

    clean up

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,8 +1,13 @@
-var slide = document.getElementById('slide')
-var sliderNum = document.getElementById('num-of-users');
-var totalCost = document.getElementById('total-cost');
+document.addEventListener('DOMContentLoaded', function () {
+  
+  // slider
+  var slide = document.getElementById('slide')
+  var sliderNum = document.getElementById('num-of-users');
+  var totalCost = document.getElementById('total-cost');
 
-slide.onchange = function() {
-  sliderNum.innerHTML = this.value;
-  totalCost.innerHTML = this.value * 17.5
-}
+  slide.onchange = function() {
+    sliderNum.innerHTML = this.value;
+    totalCost.innerHTML = this.value * 17.5
+  }
+
+}, false);
